e256a13c8068a097b00a945571251ff896d6eb06
YeJinHong <teepij@naver.com>
2024-11-02 18:10:17 +0900

    init project

---
+++ src/main/java/com/test/knockknockback/KnockKnockBackApplication.java
@@ -0,0 +1,13 @@
+package com.test.knockknockback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockKnockBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockKnockBackApplication.class, args);
+	}
+
+}
