c73bad894cbbfb454846840cfdc72fd1e6734b90
YeJinHong <teepij@naver.com>
2024-11-06 08:22:50 +0900

    fix : identifier 'hour' 사용으로 인한 컬럼명 변경

--- src/main/java/com/test/knockknockback/api/crawling/TimeDataGenerator.java
+++ src/main/java/com/test/knockknockback/api/crawling/TimeDataGenerator.java
@@ -2,7 +2,6 @@ package com.test.knockknockback.api.crawling;
 
 import com.test.knockknockback.api.booking.BookingDTO;
 import com.test.knockknockback.api.booking.BookingType;
-import com.test.knockknockback.converter.BookingConverter;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
@@ -26,14 +25,14 @@ public class TimeDataGenerator {
             String timeClass = we.getAttribute("class");
             if(timeText.contains("오후")) isAfternoon = true;
 
-            int hour = Integer.parseInt(timeText.replaceAll("[^0-9]", ""));
+            int hours = Integer.parseInt(timeText.replaceAll("[^0-9]", ""));
             BookingType type = timeClass.contains("half") ? BookingType.HALF : BookingType.FULL;
             Boolean isBookable = !timeClass.contains("disabled");
 
             bookingList.add(
                     BookingDTO.builder()
                             .crawlingTimeRequestDTO(crawlingTimeRequestDTO)
-                            .hour(hour)
+                            .hours(hours)
                             .isAfternoon(isAfternoon)
                             .isBookable(isBookable)
                             .type(type)
