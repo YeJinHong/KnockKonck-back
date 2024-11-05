33890bf99cc27a1cd05488b726646bafd053dc94
YeJinHong <teepij@naver.com>
2024-11-05 15:13:56 +0900

    feat : URL 파라미터 추출을 위한 UrlParamExtractor 구현

---
+++ src/main/java/com/test/knockknockback/api/crawling/UrlParamExtractor.java
@@ -0,0 +1,26 @@
+package com.test.knockknockback.api.crawling;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.util.StringTokenizer;
+
+@NoArgsConstructor
+@Component
+public class UrlParamExtractor {
+
+    private static final String BIZES = "bizes";
+    private static final String ITEMS = "items";
+
+    public static String getParamFromUrl(String url, String paramName){
+        StringTokenizer st = new StringTokenizer(url.substring(url.indexOf(paramName)), "[/?=&]");
+        st.nextToken();
+        return st.nextToken();
+    }
+
+    public static String getBizesNumberFromUrl(String url){
+        return getParamFromUrl(url, BIZES);
+    }
+    public static String getItemNumberFromUrl(String url){
+        return getParamFromUrl(url, ITEMS);
+    }
+}
